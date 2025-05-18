948a0ccd6c5c9e7d01649a0b0aa5a522df3f5adc
wesamAlsaleh <wesammuneer7@gmail.com>
2025-05-18 22:41:07 +0300

    Create Luma Child Theme

---
+++ app/design/frontend/child/luma_child/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::THEME, 
+    'frontend/child/luma_child', 
+    __DIR__
+);
+// app/design/frontend/child/luma_child
